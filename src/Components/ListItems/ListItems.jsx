778d67b145ca08bb73764fccd52f7a351d1f45d2
kshashank22 <kshashank.rinku@gmail.com>
2023-10-04 14:39:34 +0530

    added CRUD functionality

--- src/Components/ListItems/ListItems.jsx
+++ src/Components/ListItems/ListItems.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import './ListItems.css'
 
-const ListItems=({taskList,setTaskList})=>{
+const ListItems=({setTask,taskList,setTaskList,setEditedTask,setToggleButton})=>{
   
-  const deleteTodoTask=({id})=>{
-    setTaskList(taskList.filter((each)=>each.id!== id))
+  const deleteTodoTask=(eachTodo)=>{
+    setTaskList(taskList.filter((eachElement)=>eachElement.id!== eachTodo.id))
   };
+  
+  const updateCheckbox=(eachTodo)=>{
+    setTaskList(
+      taskList.map((eachElement)=>{
+        if(eachElement.id===eachTodo.id){
+          return{...eachElement,status:!eachElement.status}
+        }
+        return eachElement
+      })
+    )
+  }
+
+  const updatedText=(eachTodo)=>{
+    const textUpdated=taskList.find((eachElement)=>eachElement.id===eachTodo.id)
+    setEditedTask(textUpdated.id);
+    setToggleButton(false)
+    setTask(textUpdated.title)
+  }
 
   return (
     <ul>
         {taskList.map(eachTodo=>
             <li key={eachTodo.id} className='todo-list-items-container'>
                 <div>
-                    <input type="checkbox" className='todo-checkbox'/>
+                    <input type="checkbox" className='todo-checkbox' onChange={()=>updateCheckbox(eachTodo)}/>
                     <label className='todo-list-inputs'>{eachTodo.title}</label>
                 </div>
                 <div>
-                    <button type="button" className='todo-edit-buttons'>
+                    {eachTodo.status && <label className='todo-checkbox'>completed</label>}
+                    <button type="button" className='todo-edit-buttons' onClick={()=>updatedText(eachTodo)}>
                         <i className="fa-regular fa-pen-to-square todo-icon-style" style={{color:'#95dbc3'}}></i>
                     </button>
                     <button type="button" className='todo-edit-buttons' onClick={()=>deleteTodoTask(eachTodo)}>
--- src/Pages/TodoPage.jsx
+++ src/Pages/TodoPage.jsx
@@ -7,22 +7,33 @@ import ListItems from '../Components/ListItems/ListItems'
 function TodoPage() {
   const [task,setTask]=useState("")
   const [taskList,setTaskList]=useState([])
+  const [toggleButton,setToggleButton]=useState(true)
+  const [editedTask,setEditedTask]=useState(null)
   
-  const addTodoTask=(e)=>{
-    e.preventDefault()
-    if (task.trim()!==""){
+  const addTodoTask=(event)=>{
+    event.preventDefault()
+    if ((task.trim()!=="") && (!editedTask)){
         setTaskList([...taskList,{id:uuidv4(),title:task,status:false}]);
-        setTask('')
-        
-    } 
+        setTask('')   
+    }else if(task && !toggleButton){
+      setTaskList(taskList.map((eachElement)=>{
+        if(eachElement.id===editedTask){
+          return{...eachElement,title:task}
+        }
+        return eachElement
+      }))
+      setEditedTask(null)
+      setToggleButton(true)
+      setTask('')   
+    }
   }
 
-  const updatedTodoInput=(e)=>{
-       setTask(e.target.value)
+  const updatedTodoInput=(event)=>{
+    setTask(event.target.value)
   }
 
-  const clearTodoList=(e)=>{
-    e.preventDefault()
+  const clearTodoList=(event)=>{
+    event.preventDefault()
     setTaskList([])
   }
 
@@ -34,14 +45,14 @@ function TodoPage() {
             <input type="text" className='todo-input' placeholder='Your Todo' value={task} onChange={updatedTodoInput}/>
             <br/>
             <div className='todo-add-button-container'>
-                <button className="todo-add-button" type='submit' onClick={addTodoTask}>Add</button>
+                <button className="todo-add-button" type='submit' onClick={addTodoTask}>{toggleButton?"Add":"Save"}</button>
             </div>
          </form>
-         {taskList.length>0 && <ListItems taskList={taskList} setTaskList={setTaskList}/> }
+         {taskList.length>0 && <ListItems setTask={setTask} taskList={taskList} setTaskList={setTaskList} setEditedTask={setEditedTask} setToggleButton={setToggleButton}/> }
          {taskList.length>0 && <div className='todo-add-button-container'>
             <button type="button" onClick={clearTodoList}>Clear All</button>
          </div>
-}
+         }
       </div>
     </div>
   )
